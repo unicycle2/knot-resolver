5c5dbd5f2847ee4fb924ddef03024edf75c2577e
Marek Vavrusa <marek@vavrusa.com>
2016-08-22 23:22:17 -0700

    wip: multiplex outbound queries, keepalives
    this is a wip on multiplexing outbound queries
    and doing keepalives for frequently used endpoints

--- lib/layer/pktcache.c
+++ lib/layer/pktcache.c
@@ -113,7 +113,7 @@ static int pktcache_peek(kr_layer_t *ctx, knot_pkt_t *pkt)
 	    (qry->flags & QUERY_NO_CACHE)) {
 		return ctx->state; /* Already resolved/failed */
 	}
-	if (qry->ns.addr[0].ip.sa_family != AF_UNSPEC) {
+	if (qry->ns.addr[0].ip.sa_family != AF_UNSPEC && !(qry->flags & QUERY_STUB)) {
 		return ctx->state; /* Only lookup before asking a query */
 	}
 	if (knot_pkt_qclass(pkt) != KNOT_CLASS_IN) {
